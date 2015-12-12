fdd69965f302e2e8c119ef254c316e5b4f90196f
Tim Doerzbacher <tim@tim-doerzbacher.com>
2015-12-12 03:11:30 -0500

    Update boredom
    Geez... fucking MacPorts takes forever to install ImageMagick...
    wonder when I'll ever be able to test this...
    
    I mean, I could do it immediately now with another computer...
    but I'm way too stubborn to walk all the way across the room to...
    wait, everything is networked w/ SSH. I could just log...
    
    No. I'm stubborn. I shall not move.

--- boredom.js
+++ boredom.js
@@ -2,12 +2,19 @@
 
 'use strict';
 
+//
+// Required Modules
+//
+
 var fs = require('fs'),
 	path = require('path'),
 	process = require('process'),
 	_ = require('lodash'),
 	child_process = require('child_process');
 
+//
+// Find all images in current directory
+//
 var images = [];
 console.log('Looking for boring images in ' + process.cwd());
 _.forEach(fs.readdirSync(process.cwd()), function (node) {
@@ -22,23 +29,40 @@ if (images.length < 2) {
 	process.exit(50);
 }
 
+//
+// Process all images, looking for "boring" images that
+// have few pixel changes.
+//
 for (var i=1; i < images.length; i++) {
-	var args = [
-		'-metric', 'AE',
-		'-fuzz', '50%',
-		images[i-1], images[i]
-	];
-	// var output = child_process.spawnSync('compare', args);
-	// console.log(args, output);
+	(function () {
+		var delta,
+			output,
+			prev = images[i-1],
+			curr = images[i];
 
-	if (0) {
-		// this image is boring, rename it as such
-		var oldName = images[i].match(/^(.+)\.(jpeg|jpg)$/),
-			newName = oldName[1] + '-BORING.' + oldName[2];
+		output = child_process.spawnSync('compare', [
+			'-metric', 'AE',
+			'-fuzz', '50%',
+			prev, curr
+		]);
 
-		fs.renameSync(
-			path.join(process.cwd(), images[i]),
-			path.join(process.cwd(), newName)
-		);
-	}
-} // for
+		delta = parseInt(output);
+
+//		output = child_process.spawnSync('compare', {
+//			'-metric', 'MAE',
+//			prev, curr
+//		});
+//		deltaMAE = parseInt(output);
+
+		if (delta < 1000) {
+			// this image is boring, rename it as such
+			var oldName = curr.match(/^(.+)\.(jpeg|jpg)$/),
+				newName = oldName[1] + '-BORING.' + oldName[2];
+
+			fs.renameSync(
+				path.join(process.cwd(), curr),
+				path.join(process.cwd(), newName)
+			);
+		}
+	})();
+} // for(...)
